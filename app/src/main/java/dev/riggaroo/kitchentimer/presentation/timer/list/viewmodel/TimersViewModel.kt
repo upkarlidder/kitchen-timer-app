0a7f7bcb0660b675d0b756d4c4d5736a763c23fd
Rebecca Franks <franks.rebecca@gmail.com>
2020-12-23 08:41:51 +0200

    introduce import alias

--- app/src/main/java/dev/riggaroo/kitchentimer/presentation/timer/list/viewmodel/TimersViewModel.kt
+++ app/src/main/java/dev/riggaroo/kitchentimer/presentation/timer/list/viewmodel/TimersViewModel.kt
@@ -8,19 +8,20 @@ import com.spotify.mobius.runners.WorkRunner
 import com.spotify.mobius.rx2.RxMobius
 import dev.riggaroo.kitchentimer.presentation.mobius.ViewEffectConsumer
 import dev.riggaroo.kitchentimer.presentation.mobius.WorkRunnersConstants
+import com.spotify.mobius.functions.Function as MobiusFunction
 import javax.inject.Named
 
 class TimersViewModel @ViewModelInject constructor(
-    private val timerUseCase: TimersUseCase,
-    @Named(WorkRunnersConstants.MAIN_THREAD_WORK_RUNNER) workRunner: WorkRunner
-) : MobiusLoopViewModel<TimersModel, TimersEvent, TimersEffect, TimersViewEffect> (
-    com.spotify.mobius.functions.Function<ViewEffectConsumer<TimersViewEffect>, MobiusLoop.Factory<TimersModel, TimersEvent, TimersEffect>> {
-        val sideEffectHandler = TimerSideEffectHandler(timerUseCase)
-        RxMobius.loop(
-                TimersModelUpdate(),
-                sideEffectHandler
-        )
-    }, TimersModel(),
+        private val timerUseCase: TimersUseCase,
+        @Named(WorkRunnersConstants.MAIN_THREAD_WORK_RUNNER) workRunner: WorkRunner
+) : MobiusLoopViewModel<TimersModel, TimersEvent, TimersEffect, TimersViewEffect>(
+        MobiusFunction<ViewEffectConsumer<TimersViewEffect>, MobiusLoop.Factory<TimersModel, TimersEvent, TimersEffect>> {
+            val sideEffectHandler = TimerSideEffectHandler(timerUseCase)
+            RxMobius.loop(
+                    TimersModelUpdate(),
+                    sideEffectHandler
+            )
+        }, TimersModel(),
         TimersInit(),
         workRunner,
         50
