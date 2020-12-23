a7eb9d017137e20df9b9dbf839d5afa6c25991f5
Rebecca Franks <franks.rebecca@gmail.com>
2020-12-23 09:36:08 +0200

    Added basic list of timers.

--- app/src/main/java/dev/riggaroo/kitchentimer/presentation/timer/list/MainActivity.kt
+++ app/src/main/java/dev/riggaroo/kitchentimer/presentation/timer/list/MainActivity.kt
@@ -6,24 +6,39 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Icon
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.material.Text
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
 import dagger.hilt.android.AndroidEntryPoint
+import dev.riggaroo.kitchentimer.data.database.Timer
 import dev.riggaroo.kitchentimer.presentation.mobius.MobiusView
+import dev.riggaroo.kitchentimer.presentation.timer.list.viewmodel.TimersEvent
+import dev.riggaroo.kitchentimer.presentation.timer.list.viewmodel.TimersModel
+import dev.riggaroo.kitchentimer.presentation.timer.list.viewmodel.TimersViewEffect
 import dev.riggaroo.kitchentimer.presentation.timer.list.viewmodel.TimersViewModel
 import dev.riggaroo.kitchentimer.presentation.ui.*
+import java.time.Duration
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), MobiusView {
+class MainActivity : AppCompatActivity(), MobiusView<TimersModel, TimersViewEffect> {
 
     private val timerViewModel : TimersViewModel by viewModels()
 
@@ -33,9 +48,31 @@ class MainActivity : AppCompatActivity(), MobiusView {
             KitchenTimerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ListTimersScreen(modifier = Modifier.fillMaxSize()) {
-                        timerViewModel
+                    ListTimersScreen(model = timerViewModel.models, modifier = Modifier.fillMaxSize()) {
+                        timerViewModel.dispatchEvent(TimersEvent.AddTimer(Duration.ofMinutes(4), "Test Timer"))
                     }
+                }
+            }
+        }
+        subscribeToViewEffects(this, timerViewModel)
+    }
+
+    override fun handleViewEffect(viewEffect: TimersViewEffect) {
+
+    }
+}
+
+@Composable
+fun ListTimersScreen(model: LiveData<TimersModel>, modifier: Modifier, addTimerClicked: ()-> Unit) {
+    Box {
+        BackgroundGradient(modifier = modifier)
+        AddTimerButton(modifier = Modifier.align(Alignment.BottomCenter)
+                .padding(16.dp), addTimerClicked = addTimerClicked)
+        val state : TimersModel? by model.observeAsState()
+        if (state != null) {
+            LazyColumn(Modifier.padding(8.dp)) {
+                items(state!!.timers) {
+                    ListItemTimer(it)
                 }
             }
         }
@@ -43,11 +80,10 @@ class MainActivity : AppCompatActivity(), MobiusView {
 }
 
 @Composable
-fun ListTimersScreen(modifier: Modifier, addTimerClicked: ()-> Unit) {
-    Box {
-        BackgroundGradient(modifier = modifier)
-        AddTimerButton(modifier = Modifier.align(Alignment.BottomCenter)
-                .padding(16.dp), addTimerClicked = addTimerClicked)
+fun ListItemTimer(timer: Timer) {
+    Column {
+        Text(text = timer.name ?: "5 min timer")
+        Text(text = timer.durationSeconds.toString())
     }
 }
 
--- app/src/main/java/dev/riggaroo/kitchentimer/presentation/timer/list/viewmodel/TimersModelUpdate.kt
+++ app/src/main/java/dev/riggaroo/kitchentimer/presentation/timer/list/viewmodel/TimersModelUpdate.kt
@@ -2,13 +2,20 @@ package dev.riggaroo.kitchentimer.presentation.timer.list.viewmodel
 
 import com.spotify.mobius.Next
 import com.spotify.mobius.Update
+import dev.riggaroo.kitchentimer.data.database.Timer
+import java.time.ZonedDateTime
 
 class TimersModelUpdate : Update<TimersModel, TimersEvent, TimersEffect > {
 
     override fun update(model: TimersModel, event: TimersEvent): Next<TimersModel, TimersEffect> {
         return when (event) {
             is TimersEvent.AddTimer -> {
-                Next.next(model)
+                // todo invoke db call instead
+                val runningList = model.timers.toMutableList()
+                val newTimer = Timer(0, event.duration.seconds, event.name, ZonedDateTime.now())
+                runningList.add(newTimer)
+                Next.next(model.copy(timerViewState = TimerViewState.LOADED,
+                        timers = runningList))
             }
             TimersEvent.StartTimer -> TODO()
             TimersEvent.StopTimer -> TODO()
